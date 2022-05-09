2033059f75a68d052da8c824826855dd08658b19
kimson <xpfjrnseks1@naver.com>
2022-05-09 21:42:40 +0900

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { useTodoState } from './TodoContext';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+    flex: 1;
+    padding: 20px 32px;
+    padding-bottom: 48px;
+    overflow-y: auto;
+    /** background: gray; /** 사이즈 조정이 잘 되고 있는지 확인하기 위한 임시 스타일 */
+`;
+
+function TodoList() {
+    const todos = useTodoState();
+
+    return (
+        <TodoListBlock>
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    id={todo.id}
+                    text={todo.text}
+                    done={todo.done}
+                />
+            ))}
+        </TodoListBlock>
+    );
+}
+
+export default TodoList;
